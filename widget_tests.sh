5f5f91c78c4eba1921857a537551d2cfe43b4cff
donotturnoff <admin@donotturnoff.net>
2022-04-24 16:05:07 +0100

    Complete widget delay tests

--- widget_tests.sh
+++ widget_tests.sh
@@ -2,19 +2,31 @@
 
 #bin/instr_test > test/widget_tests/instr
 #bin/mem_test > test/widget_tests/mem
-bin/instr_test > instr
-bin/mem_test > mem
 
-bws=("1mbit" "10mbit" "100mbit" "1gbit" "100kbit" "10kbit")
-latencies=("0ms" "10ms" "100ms")
+#bws=("10mbit" "100mbit" "1gbit" "1mbit" "100kbit" "10kbit")
+#latencies=("0ms" "10ms" "100ms")
+
+#tc qdisc del dev enp1s0 root
+
+#for bw in $bws ; do
+#    for latency in $latencies ; do
+#        tc qdisc add dev enp1s0 root netem rate $bw delay $latency
+#        #bin/pcap_test > test/widget_tests/pcap-bw$bw-lt$latency
+#        bin/pcap_test > pcap-bw$bw-lt$latency
+#        tc qdisc del dev enp1s0 root
+#    done
+#done
 
 tc qdisc del dev enp1s0 root
-
-for bw in $bws ; do
-    for latency in $latencies ; do
-        tc qdisc add dev enp1s0 root netem rate $bw delay $latency
-        #bin/pcap_test > test/widget_tests/pcap-bw$bw-lt$latency
-        bin/pcap_test > pcap-bw$bw-lt$latency
-        tc qdisc del dev enp1s0 root
-    done
-done
+tc qdisc add dev enp1s0 root netem rate 100kbit delay 100ms
+bin/pcap_test > pcap-bw100kbit-lt100ms
+tc qdisc del dev enp1s0 root
+tc qdisc add dev enp1s0 root netem rate 10kbit delay 0ms
+bin/pcap_test > pcap-bw10kbit-lt0ms
+tc qdisc del dev enp1s0 root
+tc qdisc add dev enp1s0 root netem rate 10kbit delay 10ms
+bin/pcap_test > pcap-bw10kbit-lt10ms
+tc qdisc del dev enp1s0 root
+tc qdisc add dev enp1s0 root netem rate 10kbit delay 100ms
+bin/pcap_test > pcap-bw10kbit-lt100ms
+tc qdisc del dev enp1s0 root
